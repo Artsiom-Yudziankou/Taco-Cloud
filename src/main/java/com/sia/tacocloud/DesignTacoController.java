9b5bc4224fe57744b3c082a7ff089a47b8a515fd
arty <artyom.yudenkov@gmail.com>
2020-01-15 18:48:01 +0300

    sad story

--- src/main/java/com/sia/tacocloud/DesignTacoController.java
+++ src/main/java/com/sia/tacocloud/DesignTacoController.java
@@ -5,17 +5,17 @@ import com.sia.tacocloud.Ingredient.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
-@RequestMapping(value = "/design", method = RequestMethod.GET)
+@RequestMapping("/design")
 public class DesignTacoController {
     @GetMapping
     public String showDesignForm(Model model) {
@@ -46,4 +46,14 @@ public class DesignTacoController {
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
+
+    @PostMapping
+    public String proccessDesign (@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors())
+            return "design";
+        // Save the taco design...
+        return "redirect:/orders/current";
+    }
+
+
 }
