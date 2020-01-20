2679e092cec2f0f0c5433a4d4af646bac0c6e271
arty <artyom.yudenkov@gmail.com>
2020-01-20 12:41:38 +0300

    3-rd Chapter, JPA content. Changed Order class and orderForm.html variables.

--- src/main/java/com/sia/tacocloud/essences/jpa/Order.java
+++ src/main/java/com/sia/tacocloud/essences/jpa/Order.java
@@ -27,19 +27,19 @@ public class Order implements Serializable {
     private Date placedAt;
 
     @NotBlank(message="Name is required")
-    private String name;
+    private String deliveryName;
 
     @NotBlank(message="Street is required")
-    private String street;
+    private String deliveryStreet;
 
     @NotBlank(message = "City is required")
-    private String city;
+    private String deliveryCity;
 
     @NotBlank(message = "State is required")
-    private String state;
+    private String deliveryState;
 
     @NotBlank(message = "Zip Code is required")
-    private String zip;
+    private String deliveryZip;
 
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
